2e6e4faa5e89a341508e7e494b3d5b8e481f5477
gabriel <gabriel19oct@hotmail.com>
2020-06-05 10:09:43 -0500

    finished purple rain :)

--- purpleRain/sketch.js
+++ purpleRain/sketch.js
@@ -1,7 +1,21 @@
+
 // run seerver with Ctrl+Shift+L
 
+const drops = [];
+const numberDrops = 500
+
 function setup() {
+  createCanvas(640, 260);
+  for (let i = 0; i < numberDrops ; i++){
+    drops[i] = new Drop();
+  }
+
 }
 
 function draw() {
+  background(230, 230, 250)
+  for (let i = 0; i < numberDrops ; i++){
+    drops[i].fall();
+    drops[i].show();
+  }
 }
